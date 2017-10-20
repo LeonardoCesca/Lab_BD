0c3cfc2c9d1af4876b23cfe788cb9ad7cd96fca3
Leonardo Cesca <leonardocescaf@gmail.com>
2017-10-20 18:48:30 -0200

    20.10.17

---
+++ aula_7/exercicios_aula_7.sql
@@ -0,0 +1,38 @@
+-- EC1.   Qual a quantidade de endereços por estado?
+
+select uf, count(*)
+from estados join cidades using (uf)
+join enderecos using (cod_cidade)
+group by uf;
+
+--EC2.   Qual a quantidade de clientes de cada estado?
+
+
+--EC3.   Qual o ranking de estados por quantidade de clientes, ou seja, em ordem decrescente de quantidade de clientes?
+
+
+--EC4.   Qual o ranking de regiões por quantidade de clientes, ou seja, em ordem decrescente de quantidade de clientes?
+
+
+--EC5.   Qual a quantidade de pedidos por região?
+
+
+--EC6.   Qual a quantidade de pedidos por ano e por região, considerando apenas os pedidos feitos nos anos de 2000 até 2004?
+
+
+--EC7.   Qual o valor total gasto por cliente, ordenado em ordem decrescente de valor total?
+
+
+--EC8.   Qual o valor total gasto por cliente, ordenado em ordem decrescente de valor total, considerando apenas os clientes do Rio Grande do Sul?
+
+
+--EC9.   Qual o valor total vendido por autor?
+
+
+--EC10.  Qual o valor médio faturado com as vendas por produto?
+
+
+--EC11.  Qual o valor total de cada pedido?
+
+
+--EC12.  Qual o valor médio dos pedidos por estado?
