514cdb4992c2e362b8895760233e4eb7c427f6d8
Leonardo Cesca <leonardocescaf@gmail.com>
2017-10-20 18:13:27 -0200

    20.10.17

--- aula_7/aula_7.sql
+++ aula_7/aula_7.sql
@@ -1,3 +1,5 @@
+/* RODAR SCRIPT ESTUDO DE CASO */
+
 SELECT
  CASE nivel_privilegio
  WHEN 2 THEN 'BAIXO'
@@ -25,4 +27,43 @@ SELECT
  ELSE 'OUTROS'
  END
 FROM ADMINISTRADORES; 
+
+
+select MAX(preco) FROM PRODUTOS;
+
+select preco from produtos;
+
+select count(*) num_clientes from clientes;
+
+select count(*) from telefones;
+
+select count (ddd) from telefones where ddd = 51;
+
+CREATE TABLE PRODS
+(
+ codigo NUMERIC(3) NOT NULL,
+ nome VARCHAR(50) NOT NULL,
+ preco NUMERIC (5,2) NOT NULL,
+ tipo CHAR(1) NULL,
+ -- [S]uprimento, [C]omponente, [P]eriférico
+ CONSTRAINT PK1 PRIMARY KEY (codigo)
+); 
+
+INSERT INTO PRODS
+VALUES( 10, 'HD' ,200 ,'C');
+INSERT INTO PRODS
+VALUES( 11, 'Memoria' ,250 ,'C');
+INSERT INTO PRODS
+VALUES( 12, 'Impressora' ,680 ,'P');
+INSERT INTO PRODS
+VALUES( 13, 'Processador' ,600 ,'C');
+INSERT INTO PRODS
+VALUES( 14, 'DVD-RW' ,2 ,'S');
+INSERT INTO PRODS
+VALUES( 15, 'Papel A4' ,19 ,'S');
+INSERT INTO PRODS
+VALUES( 16, 'Scanner' ,199 ,'P'); 
+
+select * from prods;
+
 
