0f4e7e1383e6e5d24abd1e20081ce8bd9a0e83e3
Leonardo Cesca <leonardocescaf@gmail.com>
2017-10-27 18:34:36 -0200

    27/10/17

--- aula_7/exercicios_aula_7.sql
+++ aula_7/exercicios_aula_7.sql
@@ -14,12 +14,26 @@ join clientes using (cod_cliente)
 group by uf;
 
 --EC3.   Qual o ranking de estados por quantidade de clientes, ou seja, em ordem decrescente de quantidade de clientes?
-
+select uf, count(distinct cod_cliente)
+from estados join cidades using (uf)
+join enderecos using (cod_cidade)
+join clientes_enderecos using (cod_endereco)
+join clientes using (cod_cliente)
+group by uf
+order by count (distinct cod_cliente) desc;
 
 --EC4.   Qual o ranking de regiões por quantidade de clientes, ou seja, em ordem decrescente de quantidade de clientes?
 
+select regiao, count(distinct cod_cliente) 
+from estados join cidades using (uf)
+join enderecos using (cod_cidade)
+join clientes_enderecos using (cod_endereco)
+join clientes using (cod_cliente)
+group by regiao
+order by count (distinct cod_cliente) desc;
 
 --EC5.   Qual a quantidade de pedidos por região?
+
 
 
 --EC6.   Qual a quantidade de pedidos por ano e por região, considerando apenas os pedidos feitos nos anos de 2000 até 2004?
