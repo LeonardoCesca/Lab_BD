d62818a731991b8495c27fb75af92182ff0fa151
Leonardo Cesca <leonardocescaf@gmail.com>
2017-10-30 18:47:39 -0200

    30/10/2017

--- aula_7/exercicios_aula_7.sql
+++ aula_7/exercicios_aula_7.sql
@@ -44,7 +44,14 @@ order by count(num_pedido) desc;
 
 
 --EC6.   Qual a quantidade de pedidos por ano e por região, considerando apenas os pedidos feitos nos anos de 2000 até 2004?
-
+select to_char(data_emissao,'yyyy'), regiao, count(num_pedido)
+from estados join cidades using(uf)
+             join enderecos using(cod_cidade)
+             join clientes_enderecos using(cod_endereco)
+             join pedidos using(cod_cliente,cod_endereco)
+where to_char(data_emissao,'yyyy') >= '2000' and to_char(data_emissao,'yyyy') <= '2004' 
+group by to_char(data_emissao,'yyyy'), regiao
+order by to_char(data_emissao,'yyyy'), regiao;
 
 --EC7.   Qual o valor total gasto por cliente, ordenado em ordem decrescente de valor total?
 
